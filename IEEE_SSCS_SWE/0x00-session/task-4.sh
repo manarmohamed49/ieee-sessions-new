bf0975eda6b7a5aeaf47bf32634685fe0b488bee
manar <manarmohamed.icloud.com@gmail.com>
2025-04-23 22:22:19 +0200

    Fix Task 0 and Task 1 solutions for Syria and Egypt cities

--- IEEE_SSCS_SWE/0x00-session/task-4.sh
+++ IEEE_SSCS_SWE/0x00-session/task-4.sh
@@ -1,6 +1,5 @@
-mkdir -p  egypt_cities/{Alexandria,Cairo,PortSaid,Delta}
- touch egypt_cities/Alexandria/data.txt
- touch egypt_cities/Cairo/data.txt
-touch egypt_cities/PortSaid/data.txt
-touch egypt_cities/Delta/data.txt
+mkdir -p syria-cities/{Damascus,Latakia,Hama,Aleppo,Homs}
+rm -rf syria-cities
+mkdir -p egypt_cities/{Alexandria,Cairo,'Port Said',Aswan,Delta}
+touch egypt_cities/{Alexandria,Cairo,'Port Said',Aswan,Delta}/data.txt
 rm -rf egypt_cities
