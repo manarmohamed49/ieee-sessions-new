10680106e16412d1fcecf3bc89758df63a26656b
manar <manarmohamed.icloud.com@gmail.com>
2025-04-23 22:41:24 +0200

    Add task-0.sh

---
+++ IEEE_SSCS_SWE/0x00-session/task-0.sh
@@ -0,0 +1,10 @@
+   20  ssh -T git@github.com
+   21  ls
+   22  ls -l
+   23  ll
+   24  mkdir “IEEE_SSCS_SWE/0x00-session”,
+   25  cd IEEE_SSCS_SWE/0x00-session
+   26  echo "hello IEEE" >README.md
+   27  echo " hello IEEE " > README.md
+   28  history > task-0.sh
+   29  history 10 > task-0.sh
